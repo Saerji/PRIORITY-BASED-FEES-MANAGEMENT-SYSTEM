0b2f3350290454432c0bf561e816596b7b3192cb
Saerji <sergiealexism@gmail.com>
2026-04-15 22:35:25 +0800

    start

---
+++ finalproj.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <stack>
+#include <queue>
+
+struct Node {
+    std::string name;
+    std::string feeInfo;
+    float amount;
+    int urgency;
+    Node* next;
+};
+
+
+int main(void) {
+    std::cout << "Hello, World!";
+    
+    return 0;
+}
