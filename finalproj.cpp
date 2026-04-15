befdd180d95b4e9d973a102417faacf01085888e
Saerji <sergiealexism@gmail.com>
2026-04-15 22:38:27 +0800

    added menu

--- finalproj.cpp
+++ finalproj.cpp
@@ -12,9 +12,63 @@ struct Node {
     Node* next;
 };
 
+void displayMenu();
+void addFee();
+void displayFees();
+void displayByUrgency();
+void searchFee();
+void updateFee();
+void markAsPaid();
+void displayHistory();
 
 int main(void) {
-    std::cout << "Hello, World!";
-    
+    int startChoice;
+
+    std::cout << "===== Welcome to Priority-Based Fees Management System! =====\n";
+    std::cout << "1. Start\n";
+    std::cout << "2. Exit.\n\n";
+    std::cout << "Enter your choice: ";
+    std::cin >> startChoice;
+    if(startChoice == 1) {
+        displayMenu();
+    } else {
+        std::cout << "Exiting program . . .";
+        return 0;
+    }
+
     return 0;
+}
+
+void displayMenu() {
+    int choice;
+    while(true) {
+        std::cout << "==========\n";
+        std::cout << std::setw(5) << "Main Menu" << std::setw(5) << "\n";
+        std::cout << "==========\n";
+        std::cout << "1. Add Fee Record\n";
+        std::cout << "2. Show Fee Records\n";
+        std::cout << "3. Mark Fee As Paid\n";
+        std::cout << "4. Search Fee\n";
+        std::cout << "5. Display Payment History\n";
+
+        std::cout << "\n Enter your choice: ";
+        std::cin >> choice;
+
+        switch(choice) {
+            case 1: addFee(); break;
+            case 2: displayFees(); break;
+            case 3: markAsPaid(); break;
+            case 4: searchFee(); break;
+            case 5: displayHistory(); break;
+            default: {
+                std::cout << "Invalid input. Please try again.\n";
+                return;
+            }
+        }
+    }
+}
+
+
+void addFee() {
+    
 }
